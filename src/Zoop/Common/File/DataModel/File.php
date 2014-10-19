fbfcc09d84c62bd06297f8caabfef834573e42e7
crimsonronin <josh.stuart@zoopcommerce.com>
2014-10-19 16:33:40 +1100

    Added permissions to the file and image documents. Updated dependencies.

--- src/Zoop/Common/File/DataModel/File.php
+++ src/Zoop/Common/File/DataModel/File.php
@@ -14,6 +14,9 @@ use Zoop\Shard\Annotation\Annotations as Shard;
 
 /**
  * @ODM\Document
+ * @Shard\AccessControl({
+ *     @Shard\Permission\Basic(roles="*", allow="*")
+ * })
  */
 class File implements FileInterface
 {
